22ce0b01457244688930107e2f0e00e19a933205
DuchenML2301 <56047105+DuchenML2301@users.noreply.github.com>
2019-10-02 12:56:15 -0700

    hm

--- dice.py
+++ dice.py
@@ -1,3 +1,19 @@
-# Name:
-# Period
+# Name: Mackenzie Duchen
+# Period: 4
 # Dice Rolling Simulator
+import random
+
+print("How Many Times Do you Want the Six Sided Dice to Roll?: ")
+
+
+
+
+
+
+
+
+
+
+
+
+def printScore():
