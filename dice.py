bfcff3378f36eb5b4246bd691609d76ddb8ff5ff
DuchenML2301 <56047105+DuchenML2301@users.noreply.github.com>
2019-10-03 13:01:06 -0700

    ugh

--- dice.py
+++ dice.py
@@ -2,18 +2,27 @@
 # Period: 4
 # Dice Rolling Simulator
 import random
-
-print("How Many Times Do you Want the Six Sided Dice to Roll?: ")
-
-
-
-
-
-
-
-
-
-
+selected = random.randint(1,6)
+randomNum = random.randint(1,6)
+x = int(input("How Many Rolls?: "))
+print(selected)
+while True:
+	printScore()
+    print('The dice rolled and you got:')
+    x = randomNum
+    break
+if x < 0:
+        x = 0
+        print('Negative changed to zero')
+    elif x == 0:
+          print('Zero')
+    elif x == 1:
+        print('Single')
+    else:
+        print('More')
 
 
 def printScore():
+	print("The Score is: ", rolled_num)
+
+
